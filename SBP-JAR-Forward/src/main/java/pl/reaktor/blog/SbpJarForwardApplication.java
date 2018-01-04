e9bdc704222e723ec57b7bfd03bf9a9639f5360c
rperkow <rperkow@gmail.com>
2018-01-04 14:05:18 +0100

    [6] Commit: Modyfikacja pliku README.adoc oraz dodanie testow: Redirect i Forward

---
+++ SBP-JAR-Forward/src/main/java/pl/reaktor/blog/SbpJarForwardApplication.java
@@ -0,0 +1,12 @@
+package pl.reaktor.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbpJarForwardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbpJarForwardApplication.class, args);
+	}
+}
---
+++ SBP-WAR-Forward/src/main/java/pl/reaktor/blog/SbpWarForwardApplication.java
@@ -0,0 +1,12 @@
+package pl.reaktor.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbpWarForwardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbpWarForwardApplication.class, args);
+	}
+}
